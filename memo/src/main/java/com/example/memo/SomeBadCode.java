ed946d301a233047725c20b301700deddb60f10a
unknown <binarybard@pm.me>
2023-06-05 12:28:15 +0900

    feat: Refactor Deprecated Date Constructor in SomeBadCode
    - Create new Java file with bad code in memo directory
    - Define a class and method that print a message and date
    - Use deprecated constructor for date object

---
+++ memo/src/main/java/com/example/memo/SomeBadCode.java
@@ -0,0 +1,12 @@
+package com.example.memo;
+
+import java.sql.Date;
+
+public class SomeBadCode {
+    public void someBadCode() {
+        System.out.println("This is some bad code");
+
+        Date date = new Date(2020, 1, 1);
+        System.out.println(date);
+    }
+}
