a5b2cfcb4da248a9490a1d3c5a713a22be144636
BINARYBARD <binarybard@pm.me>
2023-07-05 22:08:09 +0900

    fix: CORS 필터가 필터 체인에서 잘못된 위치에 있었던 부분 수정함

--- memo/src/main/java/com/example/memo/config/security/SecurityConfig.java
+++ memo/src/main/java/com/example/memo/config/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
@@ -38,6 +39,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain restApiFilterChain(final HttpSecurity http) throws Exception {
         return http
+                .addFilterBefore(corsFilter, ChannelProcessingFilter.class)
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers("/api/auth/**", "/api/docs/**").permitAll()
@@ -52,7 +54,6 @@ public class SecurityConfig {
                         .accessDeniedHandler(jwtAccessDeniedHandler))
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new JwtAuthTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
